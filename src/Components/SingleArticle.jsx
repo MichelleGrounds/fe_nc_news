aaba86f76e6be47922e4e23b00e8d30ee78cd086
Michelle Grounds <mischoel@googlemail.com>
2019-12-13 15:31:53 +0000

    single Article voting restyled @media

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -7,17 +7,20 @@ function SingleArticle(props) {
   const { article } = props;
   return (
     <section className="singleArticleSection">
-      <div className="singleArticleBody"></div>
-      <h2 className="singleArticleh2Header">{article.title}</h2>
-      <h5 className="articleAuthorAndCreation">
-        created by {article.author} on{" "}
-        {moment(article.created_at).format("MMM Do YYYY")}
-      </h5>
-      <p>{article.body}</p>
-      <p className="singleArticleCommentsCount">
-        Comments: {article.comment_count}
-      </p>
-      <VoteUpdater article_id={article.article_id} votes={article.votes} />
+      <div className="singleArticleBodyDiv">
+        <h2 className="singleArticleh2Header">{article.title}</h2>
+        <h5 className="articleAuthorAndCreation">
+          created by {article.author} on{" "}
+          {moment(article.created_at).format("MMM Do YYYY")}
+        </h5>
+        <p>{article.body}</p>
+        <p className="singleArticleCommentsCount">
+          Comments: {article.comment_count}
+        </p>
+      </div>{" "}
+      <div className="singleArticleVoterDiv">
+        <VoteUpdater article_id={article.article_id} votes={article.votes} />
+      </div>
     </section>
   );
 }
